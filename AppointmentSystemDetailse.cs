ee3737cfb382ffeb6e110c907b268cd06beb682a
parshucodes <parshuram14234@gmail.com>
2024-02-15 22:25:44 +0530

    first commit

---
+++ AppointmentSystemDetailse.cs
@@ -0,0 +1,39 @@
+﻿namespace AppointmentSystemDetails;
+
+class Appointment
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+
+    public Appointment(string name, string description, DateTime date)
+    {
+        Name = name;
+        Description = description;
+        Date = date;
+    }
+}
+
+class AppointSystem
+{
+    private List<Appointment> items;
+
+    public AppointSystem()
+    {
+        items = new List<Appointment>();
+    }
+
+    public void AddPatient(Appointment appointment)
+    {
+        items.Add(appointment);
+    }
+
+    public void DisplayPatientInforamtion()
+    {
+        foreach (var item in items)
+        {
+            Console.WriteLine($"Patient Name:{item.Name} \nDesription: {item.Description} \nDate of Appointment:{item.Date.ToShortDateString()} \n");
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AppointmentSystemDetails;
+class Program
+{
+    static void Main(string[] args)
+    {
+        AppointSystem nose = new AppointSystem();
+        Appointment raghupati = new Appointment("raghupati", "Stomach ache", new DateTime(2024, 02, 15));
+        Appointment raghav = new Appointment("Raghav", "Mild Fever", new DateTime(2024, 02, 16));
+
+        nose.AddPatient(raghav);
+        nose.AddPatient(raghupati);
+
+        nose.DisplayPatientInforamtion();
+        Console.ReadLine();
+
+    }
+}
